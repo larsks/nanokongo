2548dc9f4d3e442ca733a3b80a43b02eaa5ed7f3
Lars Kellogg-Stedman <lars@oddbit.com>
2021-09-26 09:41:11 -0400

    Sendkeys action works

--- actions/sendkeys.go
+++ actions/sendkeys.go
@@ -3,6 +3,9 @@ package actions
 //go:generate ./genkeycodes.sh
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/bendahl/uinput"
 	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v3"
@@ -23,6 +26,40 @@ type (
 
 func (action SendKeysAction) Act(value, lastvalue int) error {
 	log.Warn().Msgf("execute sendkeys action (%d)", value)
+	for _, keyspec := range action.KeySpecs {
+		for _, modname := range keyspec.Mods {
+			log.Debug().Msgf("keydown modifier %s", modname)
+			mod, exists := keycodes[strings.ToLower(modname)]
+			if !exists {
+				log.Warn().Msgf("no such key: %s", modname)
+				return fmt.Errorf("no such key: %s", modname)
+			}
+
+			Kbd.KeyDown(mod)
+		}
+
+		for _, keyname := range keyspec.Keys {
+			log.Debug().Msgf("press key %s", keyname)
+			key, exists := keycodes[strings.ToLower(keyname)]
+			if !exists {
+				log.Warn().Msgf("no such key: %s", keyname)
+				return fmt.Errorf("no such key: %s", keyname)
+			}
+
+			Kbd.KeyPress(key)
+		}
+
+		for _, modname := range keyspec.Mods {
+			log.Debug().Msgf("keyup modifier %s", modname)
+			mod, exists := keycodes[strings.ToLower(modname)]
+			if !exists {
+				log.Warn().Msgf("no such key: %s", modname)
+				return fmt.Errorf("no such key: %s", modname)
+			}
+
+			Kbd.KeyUp(mod)
+		}
+	}
 	return nil
 }
 
